81473d2cc3ad7c6b653adfd1235a47de1eb50df4
Joe Mielzarek <joseph.mielzarek@gmail.com>
2016-08-20 17:26:34 -0500

    adjusted process.env for heroku

--- server/routes/honey.js
+++ server/routes/honey.js
@@ -1,7 +1,21 @@
 var express = require('express');
 var router = express.Router();
 var pg = require('pg');
-var connectionString = 'postgres://localhost:5432/muthree';
+var connectionString;
+
+
+
+
+// If we are running on Heroku, use the remote database (with SSL)
+if(process.env.DATABASE_URL != undefined) {
+    connectionString = process.env.DATABASE_URL + "?ssl=true";
+} else {
+    // running locally, use our local database instead
+    connectionString = 'postgres://localhost:5432/muthree';
+}
+
+
+
 
 router.get('/', function (req, res) {
 
